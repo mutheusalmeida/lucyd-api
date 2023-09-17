02f3fe3cbf19cd1ccfbc5f46b31c8e5c064024f5
Matheus Almeida <matheusalmeida016@gmail.com>
2023-09-17 14:52:32 -0400

    feat: update production datasource

--- src/main/java/lucyd/api/configuration/CorsConfiguration.java
+++ src/main/java/lucyd/api/configuration/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("https://prestige.dev").allowCredentials(true)
+            .allowedOrigins().allowCredentials(true)
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
